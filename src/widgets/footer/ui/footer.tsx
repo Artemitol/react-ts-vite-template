99394652d003048784797fe1efa7b0e63a377977
Артемий Аверин <artemiy@Artemijs-Laptop.local>
2024-11-09 00:36:08 +0300

    added footer widget

---
+++ src/widgets/footer/ui/footer.tsx
@@ -0,0 +1,10 @@
+import classes from "./header.module.scss"
+
+
+export function Footer() {
+    return (
+        <footer className={classes.header}>
+            <span>Hello World! This is React Ts application</span>
+        </footer>
+    )
+}
